9d616c8a05c9403c6d094fedcec1648bf88269f5
Naga Deepak Kaza <83226719+nagadeepak511@users.noreply.github.com>
2021-10-30 11:26:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.get('/', (req, res)=>{
 })
 
 
-app.listen(8080, ()=>{
-    console.log('Listening on port 8080')
+app.listen(process.env.PORT||8080, ()=>{
+    console.log('Listening on port '+process.env.PORT||8080)
 })
